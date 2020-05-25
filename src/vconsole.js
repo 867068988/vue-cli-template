2ca87b4d4d14d43ddb4af0befd569fb8c9f08ff3
sam-meng <867068988@qq.cm>
2020-05-26 02:22:45 +0800

    优化 vconsole 的开启方式

--- src/vconsole.js
+++ src/vconsole.js
@@ -1,46 +1,69 @@
-const tip = `移动端模拟器环境可设置 localStorage._vConsole_close = 1 来禁用 vConsole`
-
-/* dev 环境移动端直接启用 */
+/* dev 环境的移动端直接启用 */
 if (process.env.VUE_APP_ENV === 'dev') {
   if (
     /\bMobile\b/i.test(navigator.userAgent) &&
     !localStorage._vConsole_close
   ) {
-    window.console.log(tip)
-    new (require('vconsole'))()
+    const VConsole = require('vconsole')
+    window.console.log(
+      '\n移动端模拟器环境禁用 vConsole 的方法：\nlocalStorage._vConsole_close = 1\n\n',
+    )
+    new VConsole()
   }
 }
 
-/* stage 环境移动端需要在页面中快速 6 连击才会启用 */
+/* stage 环境的移动端需要在页面中快速 6 连击才会启用或禁用 */
 if (process.env.VUE_APP_ENV === 'stage') {
-  if (
-    /\bMobile\b/i.test(navigator.userAgent) &&
-    !localStorage._vConsole_close
-  ) {
-    import('vconsole').then(({ default: VConsole }) => {
-      const timeLimit = 1000 * 60 * 15 // 在指定时间内，页面刷新或重新进入时直接启用
-      if (Date.now() - localStorage._vConsole_lastDate < timeLimit) {
-        window.console.log(tip)
-        new VConsole()
-        return
-      }
-      let timer
-      let count = 0
-      const handlerEvent = function(e) {
-        clearTimeout(timer)
-        if (e.targetTouches.length === 1) {
-          count++
-        }
-        if (count >= 6) {
-          window.console.log(tip)
-          new VConsole()
-          window.removeEventListener('touchstart', handlerEvent, true)
-          localStorage._vConsole_lastDate = Date.now()
+  if (/\bMobile\b/i.test(navigator.userAgent)) {
+    let vConsole
+    const loadVconsole = async function() {
+      const { default: VConsole } = await import('vconsole')
+      vConsole = new VConsole()
+    }
+    // 启用后在指定时间内，页面刷新或重新进入时直接启用
+    if (Date.now() - localStorage._vConsole_enabledTime < 30 * 60 * 1000) {
+      loadVconsole()
+    }
+    let timer
+    let x, y
+    let count = 0
+    const reset = () => {
+      x = y = null
+      count = 0
+    }
+    const eventHandler = async function(event) {
+      clearTimeout(timer)
+      timer = setTimeout(reset, 200)
+      if (event.targetTouches.length > 1) {
+        reset()
+      } else {
+        const { clientX, clientY } = event.targetTouches[0]
+        if (x == null || y == null) {
+          x = clientX
+          y = clientY
+          count = 1
         } else {
-          timer = setTimeout(() => (count = 0), 190)
+          if (Math.abs(clientX - x) > 40 || Math.abs(clientY - y) > 40) {
+            x = clientX
+            y = clientY
+            count = 1
+          } else {
+            count++
+          }
         }
       }
-      window.addEventListener('touchstart', handlerEvent, true)
-    })
+      if (count >= 6) {
+        reset()
+        if (vConsole) {
+          vConsole.destroy()
+          vConsole = null
+          delete localStorage._vConsole_enabledTime
+        } else {
+          await loadVconsole()
+          localStorage._vConsole_enabledTime = Date.now()
+        }
+      }
+    }
+    window.addEventListener('touchstart', eventHandler, true)
   }
 }
