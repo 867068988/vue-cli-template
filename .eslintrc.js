4b9e5eb7ad9ef458a1ea68b8627008e5a8f754de
sam-meng <867068988@qq.cm>
2020-07-12 15:33:01 +0800

    同步 @vue/cli@4.4.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     ],
     'no-var': 'warn',
     'prefer-const': ['warn', { destructuring: 'all' }],
+    'no-empty': 'warn',
   },
 
   parserOptions: {
--- public/.eslintrc.js
+++ public/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   rules: {
     "no-console": "warn",
     "no-unused-vars": "warn",
-    "no-empty": "off",
+    "no-empty": "warn",
     "prettier/prettier": "warn"
   }
 };
