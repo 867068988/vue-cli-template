c623210d516f1cee02db87dd707bcd1e038cb78c
mengqinfu <867068988@qq.com>
2021-01-21 17:04:38 +0800

    dev: 路由 scrollBehavior 优化

--- FAQ.md
+++ FAQ.md
@@ -1,6 +1,6 @@
 ## 常见问题
 
 <details>
-  <summary>使用 vpn 后，可能会导致本地开发环境 ip 识别错误，出现无法热更新等问题</summary>
-  先关闭 vpn，等本地开发环境启动后，再打开 vpn
+  <summary>使用 vpn 后，可能会导致本地开发环境 ip 识别错误，出现无法热更新等问题，如何解决？</summary>
+  先关闭或挂起 vpn，等本地开发环境启动后，再打开或恢复 vpn
 </details>
--- src/router/scrollBehavior.js
+++ src/router/scrollBehavior.js
@@ -1,8 +1,19 @@
 /**
+ * @typedef {import('vue-router').Route} Route
+ * @param {Route} to
+ * @param {Route} from
+ */
+const isRouteUpdate = function(to, from) {
+  if (to.fullPath === from.fullPath) return false // 页面初始进入或刷新
+  if (from.matched.length !== to.matched.length) return false
+  return from.matched.every((record, i) => record === to.matched[i])
+}
+
+/**
  * @type {import('vue-router').RouterOptions['scrollBehavior']}
  */
 export default function(to, from, savedPosition) {
-  if (to.path !== from.path) {
+  if (!isRouteUpdate(to, from)) {
     if (savedPosition) return savedPosition
     if (to.hash) return { selector: to.hash }
     return { x: 0, y: 0 }
